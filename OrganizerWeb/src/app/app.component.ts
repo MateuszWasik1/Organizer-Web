cf39de01bf9a9ef32be40b0d9ade405f9341e420
Mateusz Wąsik <mateuszwasikkontakt@gmail.com>
2023-04-02 17:45:05 +0200

    testdata

--- OrganizerWeb/src/app/app.component.ts
+++ OrganizerWeb/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { testService } from './services/test.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'OrganizerWeb';
+  public testData: any;
+
+  constructor(private testService: testService){
+    testService.getTestData().subscribe(x => this.testData = x)
+  }
 }
