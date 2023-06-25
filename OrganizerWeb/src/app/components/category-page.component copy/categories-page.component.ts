ace0fb00f166ed19f0856369ebf9cc3bc8ea2e90
Mateusz Wąsik <mateuszwasikkontakt@gmail.com>
2023-06-25 18:26:39 +0200

    add category modal

--- OrganizerWeb/src/app/components/category-page.component copy/categories-page.component.ts
+++ OrganizerWeb/src/app/components/category-page.component copy/categories-page.component.ts
@@ -4,6 +4,7 @@ import { Store } from '@ngrx/store';
 import { AppState } from '../../app.state';
 import { loadCategories } from './categories-page-state/categories-page-state.actions';
 import { selectCategories } from './categories-page-state/categories-page-state.selectors';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-categories-page',
@@ -14,7 +15,9 @@ export class CategoriesPageComponent implements OnInit, OnDestroy {
   title = 'Kategorie - P1 - Mateusz Wąsik';
 
   public subscriptions: Subscription[];
-
+  public ShowAddModal: boolean = false;
+  public form: FormGroup = new FormGroup({});
+  
   public Categories$ = this.store.select(selectCategories)
 
   constructor(public store: Store<AppState>){
@@ -22,8 +25,17 @@ export class CategoriesPageComponent implements OnInit, OnDestroy {
   }
   ngOnInit(): void {
     this.store.dispatch(loadCategories());
-  }
 
+    this.form = new FormGroup({
+      CName: new FormControl('', {validators: [Validators.required] }),
+      CStartDate: new FormControl('', {validators: [Validators.required] }),
+      CEndDate: new FormControl('', {validators: [Validators.required] }),
+      CBudget: new FormControl('', {validators: [Validators.required] }),
+    });
+  }
+  public Save = () => {
+    let model = {}
+  }
   ngOnDestroy() {
       this.subscriptions.forEach(sub => sub.unsubscribe());
   }
