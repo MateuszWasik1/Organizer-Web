d70b0addb18e5e63625218abfa6d416693f39ad2
Mateusz Wąsik <mateuszwasikkontakt@gmail.com>
2024-04-02 22:07:52 +0200

    fix service

--- OrganizerWeb/src/app/services/tasks-subtasks.service.ts
+++ OrganizerWeb/src/app/services/tasks-subtasks.service.ts
@@ -16,18 +16,18 @@ export class TasksSubTasksService {
         let params = new HttpParams()
             .set("tGID", TGID);
 
-        return this.http.get<any>(this.apiUrl + 'api/TasksNotes', { params: params, headers: GetToken(this.cookiesService) })
+        return this.http.get<any>(this.apiUrl + 'api/TasksSubTasks/GetSubTasks', { params: params, headers: GetToken(this.cookiesService) })
     }
 
     AddTaskSubTask(model: any) : Observable<any>{
-        return this.http.post<any>(this.apiUrl + 'api/TasksNotes/AddTaskNote', model, {headers: GetToken(this.cookiesService)})
+        return this.http.post<any>(this.apiUrl + 'api/TasksSubTasks/AddTaskSubTask', model, {headers: GetToken(this.cookiesService)})
     }
 
     ChangeTaskSubTaskStatus(model: any) : Observable<any>{
-        return this.http.post<any>(this.apiUrl + 'api/TasksNotes/AddTaskNote', model, {headers: GetToken(this.cookiesService)})
+        return this.http.post<any>(this.apiUrl + 'api/TasksSubTasks/ChangeSubTaskStatus', model, {headers: GetToken(this.cookiesService)})
     }
 
-    DeleteTaskSubTask(tNGID: any) : Observable<any>{
-        return this.http.delete<any>(`${this.apiUrl}api/TasksNotes/DeleteTaskNote/${tNGID}`, { headers: GetToken(this.cookiesService)})
+    DeleteTaskSubTask(tstGID: any) : Observable<any>{
+        return this.http.delete<any>(`${this.apiUrl}api/TasksSubTasks/DeleteTaskSubTask/${tstGID}`, { headers: GetToken(this.cookiesService)})
     }
 }
